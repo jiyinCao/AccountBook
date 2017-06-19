62fa2a8d667a0bb4d720980f4ec78c12c8140afe
jiyinCao <caojy3@mail2.sysu.edu.cn>
2017-06-19 23:53:43 +0800

    Update StorageFileHelper.cs

--- AccountBook/AccountBook/StorageFileHelper.cs
+++ AccountBook/AccountBook/StorageFileHelper.cs
@@ -1,4 +1,13 @@
-﻿using System;
+IStorageFolder applicationFolder = await GetDataFolder();
+                // 根据文件名获取文件夹里面的文件
+                IStorageFile storageFile = await applicationFolder.GetFileAsync(fileName);
+                // 打开文件获取文件的数据流
+                IRandomAccessStream accessStream = await storageFile.OpenReadAsync();
+                // 使用StreamReader读取文件的内容，需要将IRandomAccessStream对象转化为Stream对象来初始化StreamReader对象
+                using (StreamReader streamReader = new StreamReader(accessStream.AsStreamForRead((int)accessStream.Size)))
+                {
+                    text = streamReader.ReadToEnd();
+                }﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -34,7 +43,16 @@ namespace AccountBook
             try
             {
                  // 获取存储数据的文件夹
-                
+                IStorageFolder applicationFolder = await GetDataFolder();
+                // 根据文件名获取文件夹里面的文件
+                IStorageFile storageFile = await applicationFolder.GetFileAsync(fileName);
+                // 打开文件获取文件的数据流
+                IRandomAccessStream accessStream = await storageFile.OpenReadAsync();
+                // 使用StreamReader读取文件的内容，需要将IRandomAccessStream对象转化为Stream对象来初始化StreamReader对象
+                using (StreamReader streamReader = new StreamReader(accessStream.AsStreamForRead((int)accessStream.Size)))
+                {
+                    text = streamReader.ReadToEnd();
+                }
             }
             catch (Exception e)
             {
@@ -45,18 +63,56 @@ namespace AccountBook
 
         public static async Task WriteFileAsync(string fileName, string content)
         {
-           
+           // 获取存储数据的文件夹
+            IStorageFolder applicationFolder = await GetDataFolder();
+            // 在文件夹里面创建文件，如果文件存在则替换掉
+            IStorageFile storageFile = await applicationFolder.CreateFileAsync(fileName, CreationCollisionOption.OpenIfExists);
+            // 使用FileIO类把字符串信息写入文件
+            await FileIO.WriteTextAsync(storageFile, content);
         }
         public static async Task WriteAsync<T>(T data, string filename)
         {
             // 获取存储数据的文件夹
-           
+           IStorageFolder applicationFolder = await GetDataFolder();
+
+            StorageFile file = await applicationFolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+            await file.DeleteAsync();
+            file = await applicationFolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+            // 获取文件的数据流来进行操作
+            using (IRandomAccessStream raStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                using (IOutputStream outStream = raStream.GetOutputStreamAt(0))
+                {
+                    // 创建序列化对象写入数据
+                    DataContractSerializer serializer = new DataContractSerializer(typeof(T));
+                    serializer.WriteObject(outStream.AsStreamForWrite(), data);
+                    await outStream.FlushAsync();
+                }
+            }
 
         }
         public static async Task<T> ReadAsync<T>(string filename)
         {
             // 获取实体类类型实例化一个对象
-            
+            T sessionState_ = default(T);
+            // 获取存储数据的文件夹
+            IStorageFolder applicationFolder = await GetDataFolder();
+            StorageFile file = await applicationFolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+            if (file == null)
+                return sessionState_;
+            try
+            {
+                using (IInputStream inStream = await file.OpenSequentialReadAsync())
+                {
+                    // 反序列化XML数据
+                    DataContractSerializer serializer = new DataContractSerializer(typeof(T));
+                    sessionState_ = (T)serializer.ReadObject(inStream.AsStreamForRead());
+                }
+            }
+            catch(Exception)
+            {
+
+            }
 
             return sessionState_;
         }
